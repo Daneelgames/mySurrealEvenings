305581e6e7f4a58d2bf4459d2298c5aae2b5c8d3
daneel <rubberandrobber@gmail.com>
2016-06-08 23:20:31 +0300

    2d scene layout

---
+++ Assets/Scripts/AnimationRandomStartTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationRandomStartTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Animator anim = GetComponent<Animator>();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+	
+}
