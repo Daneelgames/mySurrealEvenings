413dfd9ba47c2b08faa9d7c404da90b6fafb2c09
Daniil Ermakov <rubberandrobber@gmail.com>
2016-08-18 15:01:57 +0300

    fix

--- Assets/Scripts/SkillRelationController.cs
+++ Assets/Scripts/SkillRelationController.cs
@@ -5,8 +5,9 @@ using System.Collections;
 public class SkillRelationController : MonoBehaviour
 {
     public Animator anim;
+	public Canvas canvas;
     public Text _text;
-    public GameObject target;
+    public Vector3 targetPos;
     public bool active = false;
 
     void Update()
@@ -14,16 +15,25 @@ public class SkillRelationController : MonoBehaviour
         // set position to target
         if (active)
         {
-            Vector3 screenPos = Camera.main.WorldToScreenPoint(target.transform.position);
-            screenPos = new Vector3(screenPos.x, screenPos.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, screenPos, 1f);
-        }
+            //Vector3 screenPos = Camera.main.WorldToScreenPoint(target.transform.position);
+            //screenPos = new Vector3(screenPos.x, screenPos.y, transform.position.z);
+            //transform.position = Vector3.Lerp(transform.position, screenPos, 0.1f);
+            //transform.position = screenPos;
 
+            Vector3 pos;
+            float width = canvas.GetComponent<RectTransform>().sizeDelta.x;
+            float height = canvas.GetComponent<RectTransform>().sizeDelta.y;
+            float x = Camera.main.WorldToScreenPoint(targetPos).x / Screen.width;
+            float y = Camera.main.WorldToScreenPoint(targetPos).y / Screen.height;
+            pos = new Vector3(width * x - width / 2, y * height - height / 2);
+			transform.position = pos;
+        }
     }
     public void SetFeedback(GameObject trgt)
     {
-        target = trgt;
+        targetPos = trgt.transform.position;
         anim.SetTrigger("Update");
         active = true;
+        //StartCoroutine("SetInactive");
     }
 }
